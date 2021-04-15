6e0248a62498541fa88c0a174c14c182fca0cd2f
RICK <rickpro01@gmail.com>
2021-04-15 11:42:58 -0400

    Initial commit

--- app/src/main/java/com/example/myapplication21/MainActivity.java
+++ app/src/main/java/com/example/myapplication21/MainActivity.java
@@ -25,16 +25,6 @@ public class MainActivity extends AppCompatActivity {
         ingreso = (Button)findViewById(R.id.button);
         itexto = (EditText) findViewById(R.id.Itexto);
 
-        /*Bundle parametros = this.getIntent().getExtras();
-        String datos = parametros.getString("datos");
-        texto.setText(datos);*/
-
-        /*ingreso.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, )
-            }
-        });*/
     }
     public void enviarDatos(View view){
         Intent trab = new Intent (MainActivity.this,MainActivity2.class);
